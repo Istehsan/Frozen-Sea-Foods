2c92fcc3517392bd537d49e524a5e940a9199072
Istehsan <istehsanmirza@gmail.com>
2018-03-18 21:22:28 +0530

    Frozen Sea Foods V2

---
+++ FrozenSeaFoodsV2/FrozenSeaFoods/js3/Products.js
@@ -0,0 +1,7 @@
+﻿<script>
+    $(document).ready(function(){
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    $('#stuck_container').tmStickUp({});
+            $('.gallery a.gal_item').touchTouch();
+        });
+</script>
